8e5ca0e749d7c286831a6f7cac53a879f617f3a9
Akshay Gupta <akshay@merklescience.com>
2023-09-11 16:01:45 +0530

    doppler token

--- start-kafka-streaming.sh
+++ start-kafka-streaming.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 source /root/zilliqa-etl/bin/activate
-doppler run -- zilliqaetl stream --provider-uri $ZILLIQA_PROVIDER_URI -o kafka -t producer-zilliqa -ts hot -l /root/zilliqa-etl/last_synced_block.txt
+doppler run --scope '/root/zilliqa-etl' -- zilliqaetl stream --provider-uri $ZILLIQA_PROVIDER_URI -o kafka -t producer-zilliqa -ts hot -l /root/zilliqa-etl/last_synced_block.txt
