5524416e4bbf4eb51927b597001ecb4ffc0077f3
Akshay Gupta <akshay@merklescience.com>
2023-09-11 15:01:29 +0530

    start-kafka-streaming script

---
+++ start-kafka-streaming.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+source /root/zilliqa-etl/env/bin/activate
+doppler run -- zilliqaetl stream --provider-uri $ZILLIQA_PROVIDER_URI -o kafka -t producer-zilliqa -ts hot -l /root/zilliqa-etl/last_synced_block.txt
